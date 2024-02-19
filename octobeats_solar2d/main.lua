dc842fcc1f9365e7aec55f7345cf65fc57b12dfe
indi <indivicivet@gmail.com>
2024-02-19 18:02:13 +0000

    make it a regular octagon :)

--- octobeats_solar2d/main.lua
+++ octobeats_solar2d/main.lua
@@ -11,16 +11,18 @@ background.x = display.contentCenterX
 background.y = display.contentCenterY
 
 local oct_size = 100
+local a = oct_size
+local b = oct_size / (1 + math.sqrt(2))
 
 local oct_vertices = {
-    oct_size, oct_size / 2,
-    oct_size / 2, oct_size,
-    -oct_size / 2, oct_size,
-    -oct_size, oct_size / 2,
-    -oct_size, -oct_size / 2,
-    -oct_size / 2, -oct_size,
-    oct_size / 2, -oct_size,
-    oct_size, -oct_size / 2
+    a, b,
+    b, a,
+    -b, a,
+    -a, b,
+    -a, -b,
+    -b, -a,
+    b, -a,
+    a, -b
 }
 local oct = display.newPolygon(
     display.contentCenterX,
