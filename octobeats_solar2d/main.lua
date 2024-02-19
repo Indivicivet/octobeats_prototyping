175dd9b76222e5b4d4bfef50dcb02093bbbb6b32
indi <indivicivet@gmail.com>
2024-02-19 17:59:00 +0000

    center background and draw an octagon

--- octobeats_solar2d/main.lua
+++ octobeats_solar2d/main.lua
@@ -7,3 +7,25 @@
 local background = display.newImageRect(
     "octobeats_bg_0001.png", 2048, 1536
 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local oct_size = 100
+
+local oct_vertices = {
+    oct_size, oct_size / 2,
+    oct_size / 2, oct_size,
+    -oct_size / 2, oct_size,
+    -oct_size, oct_size / 2,
+    -oct_size, -oct_size / 2,
+    -oct_size / 2, -oct_size,
+    oct_size / 2, -oct_size,
+    oct_size, -oct_size / 2
+}
+local oct = display.newPolygon(
+    display.contentCenterX,
+    display.contentCenterY,
+    oct_vertices
+)
+oct.fill = {type="none"}
+oct.strokeWidth = 5
