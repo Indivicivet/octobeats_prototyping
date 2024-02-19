00ed3ae44e4ac9fb0687ec72523bb79c1e83bfd3
indi <indivicivet@gmail.com>
2024-02-19 18:12:14 +0000

    change scale (bigger) and re-layout accordingly

--- octobeats_solar2d/main.lua
+++ octobeats_solar2d/main.lua
@@ -4,13 +4,15 @@
 --
 -----------------------------------------------------------------------------------------
 
+local HEIGHT = 720
+
 local background = display.newImageRect(
-    "octobeats_bg_0001.png", 2048, 1536
+    "octobeats_bg_0001.png", HEIGHT * 3, HEIGHT * 2
 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local oct_size = 100
+local oct_size = HEIGHT * 0.5 * 0.8
 local a = oct_size
 local b = oct_size / (1 + math.sqrt(2))
 
@@ -26,15 +28,19 @@ local oct_vertices = {
 }
 local oct = display.newPolygon(
     display.contentCenterX,
-    display.contentCenterY + 20,
+    display.contentCenterY,
     oct_vertices
 )
 oct.fill = {type="none"}
-oct.strokeWidth = 5
+oct.strokeWidth = HEIGHT * 0.01
 oct.alpha = 0.7
 
 local score = 0
 
 local score_text = display.newText(
-    score, display.contentCenterX, 40, native.systemFont, 50
+    score,
+    display.contentCenterX - oct_size - HEIGHT * 0.15,
+    display.contentCenterY,
+    native.systemFont,
+    HEIGHT * 0.15
 )
